4946c2f3b4a221f1cdfa103ee937763e753033d5
Jaden Nguyen <jadenthai206@gmail.com>
2023-06-11 02:32:55 -0700

    add font

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,8 @@
-import { Text, StyleSheet, TouchableHighlight } from "react-native";
-import { heightPercentageToDP as hp } from "react-native-responsive-screen";
-import { verticalScale } from "../scales";
+import { Text, StyleSheet, TouchableHighlight } from 'react-native';
+import { heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import { verticalScale } from '../scales';
+import * as Font from 'expo-font';
+import { useEffect } from 'react';
 
 interface buttonProps {
   label?: string;
@@ -8,22 +10,28 @@ interface buttonProps {
   onPress: () => void;
 }
 
-const Button: React.FC<buttonProps> = ({
-  label,
-  outline,
-  onPress
-}) => {
+const Button: React.FC<buttonProps> = ({ label, outline, onPress }) => {
+  const loadFonts = async () => {
+    await Font.loadAsync({
+      'Proxima-Nova': require('../../assets/fonts/Proxima-Nova-Font.otf')
+    });
+  };
+
+  useEffect(() => {
+    loadFonts();
+  }, []);
 
   const styles = StyleSheet.create({
     button: {
       color: outline ? 'white' : 'rgb(99, 207, 102)',
       backgroundColor: outline ? 'rgb(99, 207, 102)' : 'white',
       textAlign: 'center',
-      lineHeight: verticalScale(41),
+      lineHeight: verticalScale(45),
       fontSize: verticalScale(20),
       height: verticalScale(45),
       borderRadius: hp('1.3%'),
-      overflow: 'hidden'
+      overflow: 'hidden',
+      fontFamily: 'Proxima-Nova'
     },
     container: {
       borderRadius: hp('1.3%')
@@ -32,18 +40,13 @@ const Button: React.FC<buttonProps> = ({
 
   const handlePress = () => {
     onPress();
-  }
+  };
 
   return (
-   <TouchableHighlight 
-     onPress={handlePress}
-     underlayColor={'transparent'}
-   >
-     <Text style={styles.button}>{label}</Text>
-   </TouchableHighlight>
+    <TouchableHighlight onPress={handlePress} underlayColor={'transparent'}>
+      <Text style={styles.button}>{label}</Text>
+    </TouchableHighlight>
   );
-  
-}
-
+};
 
 export default Button;
--- src/pages/Splash.tsx
+++ src/pages/Splash.tsx
@@ -1,102 +1,90 @@
 import { StyleSheet, Text, View, Image } from 'react-native';
-import { heightPercentageToDP  as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen';
 import Button from '../components/Button';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { horizontalScale, moderateScale, verticalScale } from '../scales';
+import * as Font from 'expo-font';
+import { useEffect } from 'react';
 
 export type TopNavigatorParamsList = {
-  Splash: undefined,
-  SignUp: undefined
-}
+  Splash: undefined;
+  SignUp: undefined;
+};
 export interface SplashProps {
   navigation: StackNavigationProp<TopNavigatorParamsList, 'Splash'>;
 }
 
 const Splash = ({ navigation }: SplashProps) => {
+  const loadFonts = async () => {
+    await Font.loadAsync({
+      'Proxima-Nova': require('../../assets/fonts/Proxima-Nova-Font.otf')
+    });
+  };
 
+  useEffect(() => {
+    loadFonts();
+  }, []);
   return (
-      <View 
-        style={styles.container}
-      >
-        <Image
-          source={require('../../assets/logo.png')}
-          style={styles.image}
-        />
-        <View style={styles.headingContainer}>
-          <Text 
-            style={[styles.heading, { color: '#8BC53F'}]}
-          >
-            FIT
-          </Text>
-          <Text 
-            style={[styles.heading, { color: '#63CF66'}]}
-          >
-            MATE
-          </Text>
-        </View>
-        <Text 
-          style={styles.subheading}
-        >
-          Your Personal Fitness Companion
-        </Text>
-        <View
-          style={styles.buttonContainer}
-        >
-          <Button 
-            label='Get started'
-            outline
-            onPress={() => navigation.navigate('SignUp')}
-          />
-          <Button 
-            label='Log in'
-            onPress={() => {}} 
-          />
-        </View>
+    <View style={styles.container}>
+      <Image source={require('../../assets/logo.png')} style={styles.image} />
+      <View style={styles.headingContainer}>
+        <Text style={[styles.heading, { color: '#8BC53F' }]}>FIT</Text>
+        <Text style={[styles.heading, { color: '#63CF66' }]}>MATE</Text>
       </View>
-  )
-}
+      <Text style={styles.subheading}>Your Personal Fitness Companion</Text>
+      <View style={styles.buttonContainer}>
+        <Button
+          label="Get started"
+          outline
+          onPress={() => navigation.navigate('SignUp')}
+        />
+        <Button label="Log in" onPress={() => {}} />
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      paddingTop: verticalScale(65),
-      backgroundColor: 'white'
-    },
-    headingContainer: {
-      textAlign: 'center',
-      textAlignVertical: 'center',
-      flexDirection: 'row',
-    },
-    background: {
-      flex: 1,
-      resizeMode: "cover",
-      zIndex: -1
-    },
-    heading: {
-      fontSize: moderateScale(80),
-      fontWeight: '600',
-      color: '#8BC53F'
-    },
-    subheading: {
-      textAlign: 'center',
-      textAlignVertical: 'center',
-      fontSize: moderateScale(17),
-      color: '#8BC53F'
-    },
-    image: {
-      width: horizontalScale(150),
-      height: verticalScale(150),
-      resizeMode: 'contain',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    buttonContainer: {
-      flex: 1,
-      gap: moderateScale(15),
-      paddingTop: verticalScale(400),
-      width: horizontalScale(380)
-    }
-  });
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    paddingTop: verticalScale(65),
+    backgroundColor: 'linear-gradient(to bottom, #ff0000, #00ff00, #0000ff)'
+  },
+  headingContainer: {
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    flexDirection: 'row'
+  },
+  background: {
+    flex: 1,
+    resizeMode: 'cover',
+    zIndex: -1
+  },
+  heading: {
+    fontSize: moderateScale(80),
+    fontWeight: '600',
+    fontFamily: 'Proxima-Nova'
+  },
+  subheading: {
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    fontSize: moderateScale(17),
+    color: '#8BC53F',
+    fontFamily: 'Proxima-Nova'
+  },
+  image: {
+    width: horizontalScale(150),
+    height: verticalScale(150),
+    resizeMode: 'contain',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonContainer: {
+    flex: 1,
+    gap: moderateScale(15),
+    paddingTop: verticalScale(400),
+    width: horizontalScale(380)
+  }
+});
 
 export default Splash;
